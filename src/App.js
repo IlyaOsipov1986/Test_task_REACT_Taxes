4a8d6516e76fb0a4fc29950417a0df2bb193854d
Илья Осипов <flasher0786@gmail.com>
2022-02-03 21:37:40 +0300

    add script

--- src/App.js
+++ src/App.js
@@ -15,14 +15,7 @@ function App() {
         <main>
           <button type={"button"} className='button__open-pop-up' onClick={() => setModalActive(true)}>Налоговый вычет</button>
         </main>
-        <Modal active={modalActive} setActive={setModalActive}
-        />
-
-
-
-
-
-
+        <Modal active={modalActive} setActive={setModalActive}/>
     </div>
 
   );
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,32 +1,49 @@
 import React, {useState} from "react";
 import './Modal.css';
 
-
 const Modal = ({active, setActive}) => {
     const [salary, setSalary] = useState('');
     const [taxesResult, setTaxesResult] = useState('');
+    const [lastTaxes, setLastTaxes] = useState('');
+    const [hidden, setHidden] = useState(true);
 
     function calcSalary () {
         let taxes = Number(salary * 12) * 0.13;
-        setTaxesResult(taxes);
-        console.log({taxes});
-        console.log({salary});
+        let lastTaxes = 260000 - (taxes * 3);
+        if (salary === '') {
+            alert('Введите сумму');
+        } else if (salary >= 55500) {
+            alert('Сумма для получения завышена');
+        } else {
+            setTaxesResult(taxes);
+            setLastTaxes(lastTaxes);
+            setHidden(false);
+            console.log(lastTaxes);
+            console.log(taxes);
+        }
     }
-    
     return (
         <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
             <div className={active ? 'modal__content active' : 'modal__content'} onClick={e => e.stopPropagation()}>
-
-                <h4 className={'modal__content-title'}>Налоговый вычет</h4>
+                <div className={'modal__content-close-btn'} onClick={() => setActive(false)}>×</div>
+                    <h4 className={'modal__content-title'}>Налоговый вычет</h4>
+                <div className={'modal__content-text'}>Используйте налоговый вычет чтобы погасить ипотеку досрочно.
+                    Размер налогового вычета составляет не более 13% от своего официального годового дохода.</div>
+                <div className={'modal__content-form'}>
                 <form>
-                    <label>Введите данные
-                        <input title={''} placeholder={'Введите данные'} value={salary} onChange={e => setSalary(e.target.value)}></input></label>
-                <button type={"button"} onClick={() => calcSalary()}>Рассчитать</button>
+                    <label>Ваша зарплата в месяц
+                       <input placeholder={'Введите данные'} value={salary} onChange={e => setSalary(e.target.value)}></input></label>
+                    <div className={'modal__content-form-btn'} onClick={() => calcSalary()}>Расситать</div>
                 </form>
-                <span>{taxesResult} рублей в 1-ый год</span>
-                <span>{taxesResult} рублей во 2-ой год</span>
-                <span>{taxesResult} рублей в 3-ий год</span>
-                <span>{taxesResult} рублей в 4-ый год </span>
+                </div>
+                <div className={hidden ? 'modal__content-result hidden' : 'modal__content-result'}>
+                    <span>{taxesResult} рублей в 1-ый год</span>
+                    <span>{taxesResult} рублей в 2-ой год</span>
+                    <span>{taxesResult} рублей в 3-ий год</span>
+                    <span>{lastTaxes} рублей в 4-ый год </span>
+                </div>
+                <div className={'modal__content-add-btn'}>Добавить</div>
+
      </div>
         </div>
     )
